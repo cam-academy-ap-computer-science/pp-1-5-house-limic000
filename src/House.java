39723116208cb1adfdd8f621ab5ee385bc7b6800
li_mic000 <li_mic000@CAM-LAB-20.bgsd.k12.wa.us>
2019-09-16 07:53:38 -0700

    Half done at school

--- src/House.java
+++ src/House.java
@@ -2,8 +2,20 @@
 public class House {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
+	}
+	
+	public static void Jack() {
+		System.out.println("This is the house that Jack built");
+	}
+	
+	public static void the() {
+		System.out.print("This is the ");
+	}
+	
+	public static void malt () {
+		System.out.println("They ate the malt");
 	}
 
 }
