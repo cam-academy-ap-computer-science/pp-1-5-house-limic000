40a524928e58dd6635bcc74ba9f7d3e135089d44
Michelle Li <limichelle009@gmail.com>
2019-09-16 16:52:30 -0700

    Finished at home!

--- src/House.java
+++ src/House.java
@@ -2,12 +2,21 @@
 public class House {
 
 	public static void main(String[] args) {
-		
-
+		v1();
+		n_line();
+		v2();
+		n_line();
+		v3();
+		n_line();
+		v4();
+		n_line();
+		v5();
+		n_line();
+		v6();
 	}
 	
-	public static void Jack() {
-		System.out.println("This is the house that Jack built");
+	public static void house() {
+		System.out.println("That lay in the house that Jack built.");
 	}
 	
 	public static void the() {
@@ -15,7 +24,79 @@ public class House {
 	}
 	
 	public static void malt () {
-		System.out.println("They ate the malt");
+		System.out.println("That ate the malt");
+	}
+	
+	public static void rat() {
+		System.out.println("That killed the rat,");
+	}
+	
+	public static void cat() {
+		System.out.println("That worried the cat,");
+	}
+	
+	public static void dog() {
+		System.out.println("That tossed the dog,");
+	}
+	
+	public static void cow() {
+		System.out.println("That milked the cow with the crumpled horn,");
+	}
+	
+	public static void n_line() {
+		System.out.println("");
 	}
 
+	public static void v1() {
+		the();
+		System.out.println("house that Jack built.");
+		n_line();
+		malt();
+		house();
+	}
+	
+	public static void v2() {
+		the();
+		System.out.println("rat,");
+		malt();
+		house();
+	}
+	
+	public static void v3() {
+		the();
+		System.out.println("cat,");
+		rat();
+		malt();
+		house();
+	}
+	
+	public static void v4() {
+		the();
+		System.out.println("dog,");
+		cat();
+		rat();
+		malt();
+		house();
+	}
+	
+	public static void v5() {
+		the();
+		System.out.println("cow with the crumpled horn,");
+		dog();
+		cat();
+		rat();
+		malt();
+		house();
+	}
+	
+	public static void v6() {
+		the();
+		System.out.println("maiden all forlorn");
+		cow();
+		dog();
+		cat();
+		rat();
+		malt();
+		house();
+	}
 }
