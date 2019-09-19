6ea55cd01ab4942a2b81e0da1df4c4f9e2f40353
li_mic000 <li_mic000@CAM-LAB-20.bgsd.k12.wa.us>
2019-09-19 07:44:12 -0700

    Re-submit!

--- src/House.java
+++ src/House.java
@@ -1,4 +1,3 @@
-
 public class House {
 
 	public static void main(String[] args) {
@@ -8,10 +7,12 @@ public class House {
 		v4();
 		v5();
 		v6();
+		v7();
 	}
 	
 	public static void house() {
 		System.out.println("That lay in the house that Jack built.");
+		n_line();
 	}
 	
 	public static void the() {
@@ -20,78 +21,71 @@ public class House {
 	
 	public static void malt () {
 		System.out.println("That ate the malt");
+		house();
 	}
 	
 	public static void rat() {
 		System.out.println("That killed the rat,");
+		malt();
 	}
 	
 	public static void cat() {
 		System.out.println("That worried the cat,");
+		rat();
 	}
 	
 	public static void dog() {
 		System.out.println("That tossed the dog,");
+		cat();
 	}
 	
 	public static void cow() {
 		System.out.println("That milked the cow with the crumpled horn,");
+		dog();
 	}
 	
 	public static void n_line() {
 		System.out.println("");
 	}
 
-	public static void repeat() {
-		malt();
-		house();
-		n_line();
-	}
 	public static void v1() {
 		the();
 		System.out.println("house that Jack built.");
 		n_line();
-		malt();
-		house();
 	}
 	
 	public static void v2() {
 		the();
-		System.out.println("rat,");
-		repeat();
+		System.out.println("malt");
+		house();
 	}
-	
 	public static void v3() {
 		the();
-		System.out.println("cat,");
-		rat();
-		repeat();
+		System.out.println("rat,");
+		malt();
 	}
 	
 	public static void v4() {
 		the();
-		System.out.println("dog,");
-		cat();
+		System.out.println("cat,");
 		rat();
-		repeat();
 	}
 	
 	public static void v5() {
 		the();
-		System.out.println("cow with the crumpled horn,");
-		dog();
+		System.out.println("dog,");
 		cat();
-		rat();
-		repeat();
 	}
 	
 	public static void v6() {
 		the();
+		System.out.println("cow with the crumpled horn,");
+		dog();
+	}
+	
+	public static void v7() {
+		the();
 		System.out.println("maiden all forlorn");
 		cow();
-		dog();
-		cat();
-		rat();
-		repeat();
 	}
 }
